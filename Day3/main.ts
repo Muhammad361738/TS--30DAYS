ac72a074d3763753605796069c393be9796179c3
Muhammad361738 <ullahehsan016@gmail.com>
2024-08-18 00:35:53 +0500

    arithematic operation , Math.random & FUNCTION RETURN TWO VALUES

---
+++ Day3/main.ts
@@ -0,0 +1,24 @@
+// Arithmetic with Mixed Types: Write a function that takes two parameters: a number and a string that represents
+//  a number (e.g., `5`). Return their sum as a number.
+ function arithematic (num1:string , num2 : number){
+    return Number(num1) + num2
+ }
+ console.log(arithematic("5",5))
+
+
+// Multiplying Decimals: Create a function that accepts two decimal numbers and returns their product.
+//  Round the result to two decimal places.
+
+function multiPlyDecimal(num1 : number,num2 :number){
+    return  Math.round(num1 * num2 * 100)/100
+}
+console.log(multiPlyDecimal(0.1,0.2))
+
+// Dividing and Finding the Remainder: Write a function that divides two numbers and returns both the quotient and 
+// the remainder. Use an object to return both values.
+function reminderAndDivision (num1 : number, num2: number):{quotient : number ; reminder:number}{
+    let quotient = Math.floor(num1/num2);
+    let reminder = num1 % num2
+    return {quotient , reminder}
+}
+console.log(reminderAndDivision(10,3))
