26f0fc1b90e84a509474be806146454825733008
Muhammad361738 <ullahehsan016@gmail.com>
2024-08-21 18:58:52 +0500

    use Numbertostring round & random Methods in function

---
+++ Day10/main.ts
@@ -0,0 +1,32 @@
+// Converting Strings to Numbers: Write a function that takes a string representation of a number
+//  (e.g., `123`) and converts it into an actual number type.
+
+function stringNumber (str :string ){
+    return Number(str);
+}
+console.log(stringNumber("125"));
+
+// Rounding Numbers: Create a function that takes a number with decimals (e.g., `3.14159`)
+//  and rounds it to two decimal places.
+function rouNumber (num : number ): number  {
+    return Math.round(num)
+}
+console.log(rouNumber(3.14159));
+
+
+// Generating Random Numbers: Write a function that generates a random number between 1 and 10, inclusive.
+function randomNumber ():number {
+    return Math.floor(Math.random() * 10 +1)
+}
+console.log(randomNumber());
+console.log(randomNumber());
+console.log(randomNumber());
+console.log(randomNumber());
+console.log(randomNumber());
+console.log(randomNumber());
+console.log(randomNumber());
+console.log(randomNumber());
+console.log(randomNumber());
+console.log(randomNumber());
+console.log(randomNumber());
+console.log(randomNumber());
