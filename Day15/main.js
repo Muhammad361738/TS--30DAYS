db342d82d14a4d3782e6f8439d38c94f924cb901
Muhammad361738 <ullahehsan016@gmail.com>
2024-08-22 23:57:53 +0500

    Write a function that returns a random boolean value, true or false

---
+++ Day15/main.js
@@ -0,0 +1,46 @@
+// Write a function that returns a random boolean value, true or false.
+function generateRandomNumber() {
+    return Math.random() > 0.5;
+}
+console.log(generateRandomNumber());
+// console.log(generateRandomNumber());
+// console.log(generateRandomNumber());
+// console.log(generateRandomNumber());
+// console.log(generateRandomNumber());
+// console.log(generateRandomNumber());
+// console.log(generateRandomNumber());
+// console.log(generateRandomNumber());
+// console.log(generateRandomNumber());
+// console.log(generateRandomNumber());
+// console.log(generateRandomNumber());
+// console.log(generateRandomNumber());
+// console.log(generateRandomNumber());
+// console.log(generateRandomNumber());
+// Create a function that generates a random hexadecimal color code.
+function randomColorGenerate() {
+    var color = "#" + Math.floor(Math.random() * 0xffffff).toString(16).padStart(6, "0");
+    return color;
+}
+console.log(randomColorGenerate());
+console.log(randomColorGenerate());
+console.log(randomColorGenerate());
+console.log(randomColorGenerate());
+console.log(randomColorGenerate());
+console.log(randomColorGenerate());
+console.log(randomColorGenerate());
+console.log(randomColorGenerate());
+console.log(randomColorGenerate());
+console.log(randomColorGenerate());
+// Simulate a dice roll using JavaScript and return a random integer between 1 and 6.
+function diceRoll() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+console.log(diceRoll());
+console.log(diceRoll());
+console.log(diceRoll());
+console.log(diceRoll());
+console.log(diceRoll());
+console.log(diceRoll());
+console.log(diceRoll());
+console.log(diceRoll());
+console.log(diceRoll());
