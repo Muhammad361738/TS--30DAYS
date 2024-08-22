dd4193107e003798efdb0b54a173086165b9696d
Muhammad361738 <ullahehsan016@gmail.com>
2024-08-22 23:41:13 +0500

    Math.sqrt() Math.random & Math.abs are used

---
+++ Day14/main.js
@@ -0,0 +1,22 @@
+// Use the JavaScript Math object to find the square root of 144.
+var square = Math.sqrt(144);
+console.log(square);
+// Generate a random integer between 1 and 10
+function randomNumberGenerate() {
+    var randonNumber = Math.floor(Math.random() * 10) + 1;
+    return randonNumber;
+}
+console.log(randomNumberGenerate());
+// console.log(randomNumberGenerate())
+// console.log(randomNumberGenerate())
+// console.log(randomNumberGenerate())
+// console.log(randomNumberGenerate())
+// console.log(randomNumberGenerate())
+// console.log(randomNumberGenerate())
+// console.log(randomNumberGenerate())
+// console.log(randomNumberGenerate())
+// console.log(randomNumberGenerate())
+// console.log(randomNumberGenerate())
+// Calculate and log the absolute difference between the number -5 and 5
+var myResult = Math.abs(-5 - 5);
+console.log(myResult);
---
+++ Day14/main.ts
@@ -0,0 +1,27 @@
+// Use the JavaScript Math object to find the square root of 144.
+const square : number = Math.sqrt(144);
+console.log(square);
+// Generate a random integer between 1 and 10
+function randomNumberGenerate () : number {
+    const randonNumber = Math.floor(Math.random() * 10 ) +1
+    return randonNumber
+}
+console.log(randomNumberGenerate())
+// console.log(randomNumberGenerate())
+// console.log(randomNumberGenerate())
+// console.log(randomNumberGenerate())
+// console.log(randomNumberGenerate())
+// console.log(randomNumberGenerate())
+// console.log(randomNumberGenerate())
+// console.log(randomNumberGenerate())
+// console.log(randomNumberGenerate())
+// console.log(randomNumberGenerate())
+// console.log(randomNumberGenerate())
+
+
+
+// Calculate and log the absolute difference between the number -5 and 5
+
+const myResult : number = Math.abs( - 5 - 5 );
+
+console.log(myResult);
