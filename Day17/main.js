92208a5e791adc168fd5c2bc67fdd2c2b6a9171f
Muhammad361738 <ullahehsan016@gmail.com>
2024-08-23 21:58:34 +0500

    use if else chain in  function

---
+++ Day17/main.js
@@ -0,0 +1,42 @@
+// Write an if statement that logs `Good Morning` if the current time is before 12 PM.
+var checkTime = new Date();
+if (checkTime.getHours() < 12) {
+    console.log("Good Morning");
+}
+// Create a function that assigns a grade (A, B, C, D, F) based on a students score.
+function gradingSystem(num) {
+    if (num >= 90) {
+        return "A+";
+    }
+    else if (num >= 80) {
+        return "A";
+    }
+    else if (num >= 70) {
+        return "B";
+    }
+    else if (num >= 60) {
+        return "C";
+    }
+    else {
+        return "Fail";
+    }
+}
+console.log(gradingSystem(55));
+// console.log(gradingSystem(93))
+// Use an if-else-if chain to categorize a persons age group (child, teenager, adult).
+function checkAge(age) {
+    if (age <= 5) {
+        return "child";
+    }
+    else if (age >= 5 && age < 18) {
+        return "Teenager";
+    }
+    else {
+        return "Adult";
+    }
+}
+console.log(checkAge(5));
+console.log(checkAge(6));
+console.log(checkAge(15));
+console.log(checkAge(18));
+console.log(checkAge(28));
