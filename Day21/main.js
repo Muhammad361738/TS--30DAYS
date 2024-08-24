a2b12fa71e92c208e40ffdb8d3e6dd027f2fab23
Muhammad361738 <ullahehsan016@gmail.com>
2024-08-24 23:56:30 +0500

    use continue

---
+++ Day21/main.js
@@ -0,0 +1,12 @@
+// Implement a for loop that counts from 1 to 10 but skips number 5 using the continue statement.
+function skipNumber() {
+    for (var i = 1; i <= 10; i++) {
+        if (i === 5) {
+            continue;
+        }
+        console.log(i);
+    }
+}
+skipNumber();
+// Use a while loop to count down from 10 to 1 and breaks the loop when it reaches 5.
+// Create a loop that iterates through a string and stops when it finds the first vowel.
---
+++ Day21/main.ts
@@ -0,0 +1,16 @@
+// Implement a for loop that counts from 1 to 10 but skips number 5 using the continue statement.
+
+function skipNumber (){
+    for (let i = 1; i <=10; i++){
+        if(i ===5){
+            continue;
+        }
+        console.log(i);
+    }
+}
+skipNumber();
+
+// Use a while loop to count down from 10 to 1 and breaks the loop when it reaches 5.
+
+
+// Create a loop that iterates through a string and stops when it finds the first vowel.
